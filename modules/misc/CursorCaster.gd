3355ac15a6981176848cc9a84f567d35176a2975
snotbane <27900656+snotbane@users.noreply.github.com>
2026-02-12 21:29:21 -0600

    ... fixup

--- modules/misc/CursorCaster.gd
+++ modules/misc/CursorCaster.gd
@@ -3,7 +3,9 @@ class_name CursorCaster3D extends RayCast3D
 
 
 @onready var camera : Camera3D = get_parent()
-@onready var length : float = target_position.length()
+
+
+@export var length : float = 100.0
 
 
 func _physics_process(delta: float) -> void:
@@ -13,7 +15,5 @@ func _physics_process(delta: float) -> void:
 	var ray_origin := camera.project_ray_origin(mouse_position)
 	var ray_normal := camera.project_ray_normal(mouse_position)
 
-	target_position = to_local(ray_normal * length)
-	global_position = ray_origin
-
+	target_position = to_local(ray_origin + ray_normal * length)
 
