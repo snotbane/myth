b715b1dfa22e30fae45f52744e80d7a96b26f7b6
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2026-02-13 05:50:51 -0600

    Added focus to CursorCaster

--- modules/misc/CursorCaster.gd
+++ modules/misc/CursorCaster.gd
@@ -2,18 +2,44 @@
 class_name CursorCaster3D extends RayCast3D
 
 
+signal focus_changed
+
+
 @onready var camera : Camera3D = get_parent()
 
 
 @export var length : float = 100.0
 
 
+var _focus : CollisionObject3D
+var focus : CollisionObject3D :
+	get: return _focus
+	set(value):
+		if _focus == value or (value != null and value is not CollisionObject3D): return
+
+		_focus = value
+		_focus_changed()
+		focus_changed.emit()
+
+
 func _physics_process(delta: float) -> void:
-	if not camera.current: return
+	if not camera.current:
+		target_position = Vector3.ZERO
+		return
 
 	var mouse_position := camera.get_viewport().get_mouse_position()
 	var ray_origin := camera.project_ray_origin(mouse_position)
 	var ray_normal := camera.project_ray_normal(mouse_position)
-
 	target_position = to_local(ray_origin + ray_normal * length)
 
+	focus = get_collider()
+
+	_physics_process_current(delta)
+
+
+## Called each physics tick, when the parent [member camera] is current, and after [member target_position] and [member focus] have been updated.
+func _physics_process_current(delta: float) -> void: pass
+
+
+## Called whenever the collided object changes.
+func _focus_changed() -> void: pass
