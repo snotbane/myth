3c63a0d236f16cfa7a129e0dded997807336a524
Liam Wofford <27900656+snotbane@users.noreply.github.com>
2026-03-12 19:16:35 -0500

    Added ScrollContainer to SettingImage

--- modules/setting/SettingImage.gd
+++ modules/setting/SettingImage.gd
@@ -7,6 +7,7 @@ var image_label : Label
 var image_button : Button
 
 var image_panel : PanelContainer
+var image_scroll_container : ScrollContainer
 var image_grid : GridContainer
 
 
@@ -31,7 +32,7 @@ func _init() -> void:
 	image_button.icon_alignment = HORIZONTAL_ALIGNMENT_FILL
 	image_button.vertical_icon_alignment = VERTICAL_ALIGNMENT_FILL
 	image_button.toggle_mode = true
-	image_button.z_index = +1
+	image_button.z_index = +2
 	hbox_panel.add_child(image_button)
 
 	image_panel = PanelContainer.new()
@@ -41,9 +42,15 @@ func _init() -> void:
 	image_panel.visible = false
 	image_button.add_child(image_panel)
 
+	image_scroll_container = ScrollContainer.new()
+	image_scroll_container.custom_minimum_size.y = BUTTON_MINIMUM_SIZE_DEFAULT.y * 5
+	image_scroll_container.vertical_scroll_mode = ScrollContainer.SCROLL_MODE_AUTO
+	image_scroll_container.horizontal_scroll_mode = ScrollContainer.SCROLL_MODE_DISABLED
+	image_panel.add_child(image_scroll_container)
+
 	image_grid = GridContainer.new()
 	image_grid.columns = 5
-	image_panel.add_child(image_grid)
+	image_scroll_container.add_child(image_grid)
 
 	image_button.toggled.connect(_image_button_toggled)
 	image_panel.visibility_changed.connect(_image_panel_visibility_changed)
@@ -60,9 +67,6 @@ var _image : Texture2D
 
 		_value_changed()
 
-@export var grid_columns : int = 5 :
-	get: return image_grid.columns
-	set(value): image_grid.columns = value
 
 var _button_size_min := BUTTON_MINIMUM_SIZE_DEFAULT
 @export var button_size_min := BUTTON_MINIMUM_SIZE_DEFAULT :
@@ -71,10 +75,21 @@ var _button_size_min := BUTTON_MINIMUM_SIZE_DEFAULT
 		_button_size_min = value
 
 		image_button.custom_minimum_size = _button_size_min
+		image_scroll_container.custom_minimum_size.y = button_size_min.y * _grid_rows
 
 		for child: Button in image_grid.get_children():
 			child.custom_minimum_size = _button_size_min
 
+@export var grid_columns : int = 5 :
+	get: return image_grid.columns
+	set(value): image_grid.columns = value
+
+var _grid_rows : int = 5
+@export var grid_rows : int = 5 :
+	get: return _grid_rows
+	set(value):
+		_grid_rows = value
+		image_scroll_container.custom_minimum_size.y = button_size_min.y * value
 
 func _ready() -> void:
 	refresh()
